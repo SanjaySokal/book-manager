547f668f8c4d16eebeb62c43e32243e82c0fa104
Sanjay Sokal <sokalsanjay@gmail.com>
2023-12-06 07:27:26 +0530

    All Api is working

--- manager/src/main/java/com/book/manager/ApiController/BooksController.java
+++ manager/src/main/java/com/book/manager/ApiController/BooksController.java
@@ -1,7 +1,9 @@
 package com.book.manager.ApiController;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,31 @@ public class BooksController {
 
     @GetMapping("/")
     private String hello() {
-        return "Hello";
+        return "Hii Welcome to the app";
+    }
+
+    @GetMapping("/books")
+    private List<Books> getBooks() {
+        return service.getAllBooks();
     }
 
     @PostMapping("/add")
     private boolean addBook(@RequestBody Books book) {
         return service.addBook(book);
+    }
+
+    @PostMapping("/update")
+    private boolean updateBook(@RequestBody Books book) {
+        return service.addBook(book);
+    }
+
+    @GetMapping("/delete/{id}")
+    private boolean deleteBook(@PathVariable Long id) {
+        return service.deleteBook(id);
+    }
+
+    @PostMapping("/delete")
+    private boolean deleteOneBook(@RequestBody Books books) {
+        return service.deleteBook(books);
     }
 }
